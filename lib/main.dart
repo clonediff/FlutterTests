0ece8514ebc91e545bc8b2de53050c4bf1fa6c26
Rail Bariev <r.bariev@hightech.group>
2024-04-02 16:42:25 +0300

    [add] Adding Assets

--- lib/main.dart
+++ lib/main.dart
@@ -3,81 +3,40 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyFirstApp());
+  runApp(MyFirstApp());
 }
 
-class MyFirstApp extends StatefulWidget {
+class MyFirstApp extends StatelessWidget {
   const MyFirstApp({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return _MyFirstAppState();
-  }
-}
-
-class _MyFirstAppState extends State<MyFirstApp> {
-  bool _loading = false;
-  double _progressValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'IndieFlower'),
       home: Scaffold(
-        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("My First App"),
-          centerTitle: true,
-          titleTextStyle: const TextStyle(
-              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+          title: const Text('Adding Assets'),
         ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      LinearProgressIndicator(
-                        value: _progressValue,
-                      ),
-                      Text(
-                        '${(_progressValue * 100).round()} %',
-                        style: const TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ],
-                  )
-                : const Text(
-                    'Press button to download',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              const Image(
+                image: AssetImage('assets/images/bg.jpg'),
+              ),
+              Image.asset('assets/icons/icon.png'),
+              const Positioned(
+                top: 16,
+                left: 100,
+                child: Text(
+                  'My custom font',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+            ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: const Icon(Icons.cloud_download),
         ),
       ),
     );
-  }
-
-  void _updateProgress() {
-    const oneSec = Duration(milliseconds: 100);
-    Timer.periodic(oneSec, (timer) {
-      setState(() {
-        _progressValue += 0.1;
-
-        if (_progressValue.toStringAsFixed(1) == '1.0') {
-          _loading = false;
-          timer.cancel();
-          _progressValue = 0.0;
-        }
-      });
-    });
   }
 }
