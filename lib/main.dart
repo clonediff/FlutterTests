41980a018b898a830787cf5d7f7d7aa3db89bad6
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-09 15:43:35 +0300

    [add] Padding widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,87 +13,31 @@ class MyFirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromRGBO(120, 137, 207, 1),
-        appBar: AppBar(
-          title: const Text('Counter'),
-          backgroundColor: const Color.fromRGBO(63, 84, 190, 1),
+        appBar: DefaultAppBar(
+          title: const Text('Building layouts'),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(16),
+          child: Text(
+            'Hello flutter!',
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DefaultAppBar extends AppBar {
+  DefaultAppBar({super.key, required Widget title})
+      : super(
+          title: title,
+          backgroundColor: Colors.blue,
           centerTitle: true,
           titleTextStyle: const TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
             fontSize: 20,
           ),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Tap "-" to decrement',
-                style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              CounterWidget(),
-              Text(
-                'Tap "+" to increment',
-                style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CounterWidget extends StatefulWidget {
-  const CounterWidget({super.key});
-
-  @override
-  State<StatefulWidget> createState() => _CounterWidgetState();
-}
-
-class _CounterWidgetState extends State<CounterWidget> {
-  int _counter = 50;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      height: 50,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(197, 202, 236, 1),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          IconButton(
-            onPressed: () => setState(() {
-              _counter--;
-            }),
-            icon: const Icon(Icons.remove),
-          ),
-          Text(
-            "$_counter",
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          IconButton(
-            onPressed: () => setState(() {
-              _counter++;
-            }),
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-    );
-  }
+        );
 }
