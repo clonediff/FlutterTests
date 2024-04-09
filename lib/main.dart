c677fb32d2f83a6b2a528791f916aa46d4492836
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-09 17:04:56 +0300

    [add] Практика. Компоновка интерфейса

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:async';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyFirstApp());
@@ -15,38 +11,176 @@ class MyFirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: DefaultAppBar(
-          title: const Text('Building layouts'),
-        ),
-        body: const Stack(
-          alignment: Alignment(0, 0),
-          children: [
-            Icon(Icons.airplay, color: Colors.red, size: 200),
-            Positioned(
-              top: 60,
-              left: 45,
-              child: Text(
-                'TV',
-                style: TextStyle(fontSize: 30),
-              ),
-            )
+        appBar: AppBar(
+          title: const Text(
+            'Weather',
+            style: TextStyle(color: Colors.black87),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          iconTheme: const IconThemeData(color: Colors.black54),
+          shape: const Border(bottom: BorderSide(color: Colors.grey, width: 1)),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
           ],
         ),
+        body: _buildBody(),
       ),
     );
   }
 }
 
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        _headerImage(),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _weatherDescription(),
+                const Divider(),
+                _temperature(),
+                const Divider(),
+                _temperatureForecast(),
+                const Divider(),
+                _footerRatings()
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Image _headerImage() {
+  return Image.network(
+    'https://img4.goodfon.ru/original/800x480/e/c5/priroda-oblaka-solnyshko-iasnaia-pogoda.jpg',
+    fit: BoxFit.cover,
+  );
+}
+
+Column _weatherDescription() {
+  return const Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Text(
+        'Tuesday - May 22',
+        style: TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Divider(),
+      Text(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio ligula, sagittis ut mi vel, tincidunt porttitor urna. Proin eu pretium diam. Curabitur gravida diam volutpat, fermentum nunc nec, accumsan odio.',
+        style: TextStyle(color: Colors.black54),
+      )
+    ],
+  );
+}
+
+Row _temperature() {
+  return const Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      SizedBox(width: 16),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                '15° Clear',
+                style: TextStyle(color: Colors.deepPurple),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                'Murmanskaya oblast, Murmansk',
+                style: TextStyle(color: Colors.grey),
+              )
+            ],
+          )
+        ],
+      ),
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10,
+    children: List.generate(8, (index) {
+      return Chip(
+        label: Text(
+          '${index + 20}° C',
+          style: const TextStyle(fontSize: 12),
+        ),
+        avatar: Icon(
+          Icons.wb_cloudy,
+          color: Colors.blue.shade300,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+          side: const BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      );
+    }),
+  );
+}
+
+Row _footerRatings() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      const Text(
+        'Info with openweathermap.org',
+        style: TextStyle(fontSize: 15),
+      ),
+      Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.star, size: 15, color: Colors.yellow.shade600),
+          Icon(Icons.star, size: 15, color: Colors.yellow.shade600),
+          Icon(Icons.star, size: 15, color: Colors.yellow.shade600),
+          const Icon(Icons.star, size: 15, color: Colors.black),
+          const Icon(Icons.star, size: 15, color: Colors.black),
+        ],
+      ),
+    ],
+  );
+}
+
 class DefaultAppBar extends AppBar {
   DefaultAppBar({super.key, required Widget title})
       : super(
-          title: title,
-          backgroundColor: Colors.blue,
-          centerTitle: true,
-          titleTextStyle: const TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        );
+    title: title,
+    backgroundColor: Colors.blue,
+    centerTitle: true,
+    titleTextStyle: const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+    ),
+  );
 }
