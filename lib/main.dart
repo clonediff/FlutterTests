f2d034f133706048777a789eac72827b03314354
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-11 09:50:58 +0300

    [add] ListView with headings

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,57 @@ class MyFirstApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> items = List<String>.generate(10000, (i) => 'Item #$i');
+    final List<ListItem> items = List<ListItem>.generate(
+        10000,
+        (i) => i % 6 == 0
+            ? HeadingItem(title: 'Item #$i')
+            : MessageItem(
+                sender: 'Sender $i',
+                body: 'Message body $i',
+              ));
 
     return MaterialApp(
       home: Scaffold(
           appBar: DefaultAppBar(
             title: const Text('Building List View'),
           ),
-          body: ListView.builder(
-            itemBuilder: (context, index) => Card(
-              child: ListTile(
-                title: Text(items[index]),
-                leading: const Icon(Icons.insert_photo, color: Colors.red),
-                trailing: const Icon(Icons.keyboard_arrow_right),
-              ),
-            ),
-          )),
+          body: ListView.builder(itemBuilder: (context, index) {
+            switch (items[index]) {
+              case HeadingItem headingItem:
+                return ListTile(
+                  title: Text(
+                    headingItem.title,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                );
+              case MessageItem messageItem:
+                return ListTile(
+                  title: Text(messageItem.sender),
+                  subtitle: Text(messageItem.body),
+                  leading: const Icon(Icons.insert_photo, color: Colors.red),
+                  trailing: const Icon(Icons.keyboard_arrow_right),
+                );
+              default:
+                throw UnimplementedError('Неизвестный тип элемента: ${items.runtimeType}');
+            }
+          })),
     );
   }
+}
+
+abstract class ListItem {}
+
+class HeadingItem extends ListItem {
+  final String title;
+
+  HeadingItem({required this.title});
+}
+
+class MessageItem extends ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem({required this.sender, required this.body});
 }
 
 class DefaultAppBar extends AppBar {
