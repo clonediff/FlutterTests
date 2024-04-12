179f8cbb49788052a0323dfb92c9e064d57b59e0
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-12 18:18:37 +0300

    [add] Cubit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_project/bloc/user_bloc.dart';
 import 'package:test_project/bloc/user_event.dart';
+import 'package:test_project/bloc/user_state.dart';
+import 'package:test_project/cubit/internet_cubit.dart';
 import 'package:test_project/default_appbar.dart';
 import 'package:test_project/services/user_repository.dart';
 import 'package:test_project/widgets/action_buttons.dart';
@@ -14,13 +16,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepositoryProvider(
       create: (context) => UserRepository(),
-      child: BlocProvider(
-        create: (context) =>
-            UserBloc(userRepository: context.read<UserRepository>())
-              ..add(UserLoadEvent()),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) =>
+                UserBloc(userRepository: context.read<UserRepository>())
+                  ..add(UserLoadEvent()),
+          ),
+          BlocProvider(
+            create: (context) => ConnectionCubit(),
+          )
+        ],
         child: Scaffold(
           appBar: DefaultAppBar(
-            title: const Text('User list'),
+            title: BlocBuilder<ConnectionCubit, MyConnectionState>(
+              builder: (context, state) => state.connected
+                  ? const Text('User List (в сети)')
+                  : const Text(
+                      'Нет соединения с интернетом',
+                      style: TextStyle(color: Colors.red),
+                    ),
+            ),
           ),
           backgroundColor: Colors.white,
           body: const Column(
