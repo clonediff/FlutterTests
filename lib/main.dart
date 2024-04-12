6df2c84733447dcf5a8eebf5488a5e19e1f38b24
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-12 15:55:13 +0300

    [add] Manual Json Serialize/Deserialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/pages/register_form_page.dart';
+import 'package:test_project/default_appbar.dart';
+import 'package:test_project/offices.dart';
 
 void main() {
   runApp(const MyFirstApp());
@@ -11,7 +12,57 @@ class MyFirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: RegisterFormPage(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<OfficesList> officesList;
+
+  @override
+  void initState() {
+    super.initState();
+    officesList = getOfficesList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DefaultAppBar(
+        title: const Text('Manual JSON Serialization'),
+      ),
+      body: FutureBuilder<OfficesList>(
+        future: officesList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data?.offices.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text('${snapshot.data?.offices[index].name}'),
+                      subtitle:
+                      Text('${snapshot.data?.offices[index].address}'),
+                      leading: Image.network(
+                          '${snapshot.data?.offices[index].image}'),
+                      isThreeLine: true,
+                    ),
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return const Text('Error');
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
--- lib/model/user.dart
+++
@@ -1,7 +0,0 @@
-class User {
-  late String name;
-  late String phone;
-  String? email;
-  String? country;
-  String? story;
-}
