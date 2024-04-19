eb48c21d1a79980879e9196aef545ac5988df25a
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-19 20:49:57 +0300

    [add] InheritedWidget example

--- lib/main.dart
+++ lib/main.dart
@@ -22,71 +22,102 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _rating = 0;
+  int _counter = 0;
+
+  int get counterValue => _counter;
+
+  void _incrementCounter() => setState(() => _counter++);
+
+  void _decrementCounter() => setState(() => _counter--);
 
   @override
   Widget build(BuildContext context) {
-    double _size = 50;
-
     return Scaffold(
       appBar: DefaultAppBar(
-        title: const Text('Vanilla Demo'),
+        title: const Text('Inherited Widget'),
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: _rating >= 1
-                  ? const Icon(
-                      Icons.star
-                    )
-                  : const Icon(
-                      Icons.star_border
-                    ),
-              color: Colors.indigo.shade500,
-              iconSize: _size,
-              onPressed: (){
-                setState(() {
-                  _rating = 1;
-                });
-              },
-            ),
-            IconButton(
-              icon: _rating >= 2
-                  ? const Icon(
-                  Icons.star
-              )
-                  : const Icon(
-                  Icons.star_border
-              ),
-              color: Colors.indigo.shade500,
-              iconSize: _size,
-              onPressed: (){
-                setState(() {
-                  _rating = 2;
-                });
-              },
-            ),
-            IconButton(
-              icon: _rating >= 3
-                  ? const Icon(
-                  Icons.star
-              )
-                  : const Icon(
-                  Icons.star_border
-              ),
-              color: Colors.indigo.shade500,
-              iconSize: _size,
-              onPressed: (){
-                setState(() {
-                  _rating = 3;
-                });
-              },
-            ),
-          ],
-        ),
+      body: MyInheritedWidget(
+        myState: this,
+        child: const AppRootWidget(),
       ),
     );
+  }
+}
+
+class AppRootWidget extends StatelessWidget {
+  const AppRootWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final rootWidgetState = MyInheritedWidget.of(context);
+    return Card(
+      elevation: 4,
+      child: Column(
+        children: [
+          Text('(Root widget)',
+              style: Theme.of(context).textTheme.displayLarge),
+          Text('${rootWidgetState._myState.counterValue}',
+              style: Theme.of(context).textTheme.displayLarge),
+          const SizedBox(height: 50),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Counter(),
+              Counter(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Counter extends StatelessWidget {
+  const Counter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final rootWidgetState = MyInheritedWidget.of(context);
+    return Card(
+      margin: const EdgeInsets.all(4).copyWith(bottom: 32),
+      color: Colors.yellowAccent,
+      child: Column(
+        children: [
+          const Text('(Child Widget)'),
+          Text('${rootWidgetState._myState.counterValue}',
+              style: Theme.of(context).textTheme.displayLarge),
+          ButtonBar(
+            children: [
+              IconButton(
+                onPressed: () => rootWidgetState._myState._decrementCounter(),
+                icon: const Icon(Icons.remove),
+                color: Colors.red,
+              ),
+              IconButton(
+                onPressed: () => rootWidgetState._myState._incrementCounter(),
+                icon: const Icon(Icons.add),
+                color: Colors.green,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyInheritedWidget extends InheritedWidget {
+  final _HomePageState _myState;
+
+  const MyInheritedWidget({super.key, required super.child, required myState})
+      : _myState = myState;
+
+  @override
+  bool updateShouldNotify(MyInheritedWidget oldWidget) {
+    return _myState.counterValue != oldWidget._myState.counterValue;
+  }
+
+  static MyInheritedWidget of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType()!;
   }
 }
