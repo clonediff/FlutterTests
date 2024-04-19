548525615dd2942c6e8ee4d6b48385019218e086
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-19 19:43:01 +0300

    [add] SetState Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/pages/home_page.dart';
+import 'package:test_project/default_appbar.dart';
 
 void main() {
   runApp(const MyFirstApp());
@@ -10,8 +10,83 @@ class MyFirstApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage()
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _rating = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    double _size = 50;
+
+    return Scaffold(
+      appBar: DefaultAppBar(
+        title: const Text('Vanilla Demo'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: _rating >= 1
+                  ? const Icon(
+                      Icons.star
+                    )
+                  : const Icon(
+                      Icons.star_border
+                    ),
+              color: Colors.indigo.shade500,
+              iconSize: _size,
+              onPressed: (){
+                setState(() {
+                  _rating = 1;
+                });
+              },
+            ),
+            IconButton(
+              icon: _rating >= 2
+                  ? const Icon(
+                  Icons.star
+              )
+                  : const Icon(
+                  Icons.star_border
+              ),
+              color: Colors.indigo.shade500,
+              iconSize: _size,
+              onPressed: (){
+                setState(() {
+                  _rating = 2;
+                });
+              },
+            ),
+            IconButton(
+              icon: _rating >= 3
+                  ? const Icon(
+                  Icons.star
+              )
+                  : const Icon(
+                  Icons.star_border
+              ),
+              color: Colors.indigo.shade500,
+              iconSize: _size,
+              onPressed: (){
+                setState(() {
+                  _rating = 3;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/services/user_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:test_project/model/user.dart';
-import 'package:test_project/services/user_api_provider.dart';
-
-class UserRepository {
-  final UserProvider _userProvider = UserProvider();
-
-  Future<List<User>> getAllUsers() => _userProvider.getUsers();
-}
