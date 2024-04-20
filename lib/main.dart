89e15d888b58dd9354dca127c9f9d6c3004ece36
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-20 22:42:56 +0300

    [add] Local files read/write examples

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'dart:convert';
-import 'dart:math';
+import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyFirstApp());
@@ -15,73 +14,126 @@ class MyFirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Providers HW',
+      title: 'Local file read/write Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: ChangeNotifierProvider<ColorsState>(
-        create: (BuildContext context) => ColorsState(),
-        child: Consumer<ColorsState>(
-          builder: (context, state, child) => Scaffold(
-            appBar: AppBar(
-              title: const Text('Providers HW'),
-              centerTitle: true,
-              backgroundColor: Colors.black,
-              foregroundColor: state.appBarColor,
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  AnimatedContainer(
-                    duration: const Duration(milliseconds: 500),
-                    height: 100,
-                    width: 100,
-                    color: state.rectColor,
-                  ),
-                  Switch(
-                    value: state.switchEnabled,
-                    onChanged: (value) => state.genNewColors(value),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: const ReadWriteFileExample(),
     );
   }
 }
 
-class ColorsState extends ChangeNotifier {
-  final Random _random = Random();
+class ReadWriteFileExample extends StatefulWidget {
+  const ReadWriteFileExample({super.key});
 
-  late Color _appBarColor;
+  @override
+  State<ReadWriteFileExample> createState() => _ReadWriteFileExampleState();
+}
 
-  Color get appBarColor => _appBarColor;
+class _ReadWriteFileExampleState extends State<ReadWriteFileExample> {
+  final TextEditingController _textController = TextEditingController();
 
-  late Color _rectColor;
+  static const String kLocalFileName = 'demo_localfile.txt';
 
-  Color get rectColor => _rectColor;
+  String _localFileContent = '';
+  String _localFilePath = '';
 
-  bool _switchEnabled = false;
-
-  bool get switchEnabled => _switchEnabled;
-
-  ColorsState() {
-    _appBarColor = _randomColor();
-    _rectColor = _randomColor();
+  @override
+  void initState() {
+    super.initState();
+    _readFromLocalFile();
+    _getLocalFile.then((file) => setState(() => _localFilePath = file.path));
   }
 
-  void genNewColors(bool switchValue) {
-    _appBarColor = _randomColor();
-    _rectColor = _randomColor();
-    _switchEnabled = switchValue;
-    notifyListeners();
+  @override
+  Widget build(BuildContext context) {
+    FocusNode textFieldFocusNode = FocusNode();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Write to local file:',
+          style: TextStyle(fontSize: 20),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          const Text(
+            'Write to local file:',
+            style: TextStyle(fontSize: 20),
+          ),
+          TextField(
+            focusNode: textFieldFocusNode,
+            controller: _textController,
+            maxLines: null,
+            style: const TextStyle(fontSize: 20),
+          ),
+          ButtonBar(
+            children: [
+              MaterialButton(
+                onPressed: () async {
+                  _readFromLocalFile();
+                  _textController.text = _localFileContent;
+                  FocusScope.of(context).requestFocus(textFieldFocusNode);
+                  log('String successfully loaded from local file');
+                },
+                child: const Text(
+                  'Load',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              MaterialButton(
+                onPressed: () async {
+                  await _writeLocalFile(_textController.text);
+                  _textController.clear();
+                  await _readFromLocalFile();
+                  log('String successfully written to local file');
+                },
+                child: const Text(
+                  'Save',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+          const Divider(height: 20),
+          Text('Local file path:',
+              style: Theme.of(context).textTheme.titleMedium),
+          Text(_localFilePath, style: Theme.of(context).textTheme.titleSmall),
+          const Divider(height: 20),
+          Text('Local file content:',
+              style: Theme.of(context).textTheme.titleMedium),
+          Text(_localFileContent,
+              style: Theme.of(context).textTheme.titleSmall),
+        ],
+      ),
+    );
   }
 
-  Color _randomColor() => Color.fromARGB(
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-      );
+  Future<String> get _getLocalPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _getLocalFile async {
+    final path = await _getLocalPath;
+    return File('$path/$kLocalFileName');
+  }
+
+  Future<File> _writeLocalFile(String text) async {
+    final file = await _getLocalFile;
+    return file.writeAsString(text);
+  }
+
+  Future _readFromLocalFile() async {
+    String content;
+    try {
+      final file = await _getLocalFile;
+      content = await file.readAsString();
+    } catch (e) {
+      content = 'Error loading local file: $e';
+    }
+    setState(() {
+      _localFileContent = content;
+    });
+  }
 }
