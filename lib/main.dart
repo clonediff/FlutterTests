1a6b753dea0113c782b3e001470a59a2caa6eac5
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-23 17:04:21 +0300

    [add] Freezed with BLoC Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/models/location.dart';
-import 'package:test_project/models/user_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_project/bloc/counter_bloc.dart';
+import 'package:test_project/pages/counter_page.dart';
 
 void main() {
-  User userA = User(name: 'User A', age: 30, locations: [Location(zipcode: '59601')]);
-  print(userA.toJson());
-
-  // User userA = User(name: 'User', age: 30);
-  // User userB = User(name: 'User', age: 30);
-  // print(userA.hashCode);
-  // print(userB.hashCode);
-  // print(userA == userB);
-  //
-  // print('User A: $userA');
-  //
-  // final userCopyA = userA.copyWith(
-  //   name: 'User A',
-  //   age: null,
-  // );
-  // print('User A Copy: $userCopyA');
-  //
-  // final json = userA.toJson();
-  // print('User A toJson: $json');
-  // print('User A fromJson: ${User.fromJson(json)}');
-  //
-  // print(userA.infoUser());
-
-  // runApp(const MyFirstApp());
+  runApp(const MyFirstApp());
 }
 
 // flutter pub run build_runner watch --delete-conflicting-outputs
@@ -36,10 +14,13 @@ class MyFirstApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: const Placeholder(),
+    return BlocProvider(
+      create: (context) => CounterBloc()..add(const CounterEvent.start()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        home: const CounterPage(),
+      ),
     );
   }
 }
