3a1aef25c0e5ec86808bd1ecd54ec9f16ee3a6fa
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-25 10:34:05 +0300

    [add] WebViewController Basics Example

--- lib/page/webview_page.dart
+++ lib/page/webview_page.dart
@@ -1,53 +1,138 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewPage extends StatelessWidget {
+enum MenuOptions {
+  clearCache,
+  clearCookies,
+}
+
+class WebViewPage extends StatefulWidget {
   const WebViewPage({super.key});
 
   @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  final WebViewController _webController = WebViewController();
+  double progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _webController
+      ..loadRequest(Uri.parse('https://flutter.dev'))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (progress) {
+            this.progress = progress / 100;
+            setState(() {});
+          },
+        ),
+      );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('WebView'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.arrow_back_ios),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.arrow_forward_ios),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.replay),
-          ),
-        ],
-      ),
-      body: WebViewWidget(
-        controller: WebViewController()
-        // HTTPS
-        // Работает по умолчанию
-          ..loadRequest(Uri.parse('https://flutter.dev'))
-        // HTTP
-        // Необходимо добавить android:usesCleartextTraffic="true" в /android/app/src/main/AndroidManifest.xml
-        // Необходимо добавить в /ios/Runner/Info.plist
-        /*
-        <key>UIViewControllerBasedStatusBarAppearance</key>
-        <false/>
-        <key>NSAppTransportSecurity</key>
-        <dict>
-            <key>NSAllowsArbitraryLoads</key>
-            <true/>
-        </dict>
-         */
-        //   ..loadRequest(Uri.parse('http://smotret-tv.live/eurosport-1.html'))
-          ..setJavaScriptMode(JavaScriptMode.unrestricted),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.next_plan, size: 32),
-        onPressed: () {},
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (_) async {
+        if (await _webController.canGoBack()) {
+          await _webController.goBack();
+        } else {
+          log('Нет записи в истории');
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('WebView'),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                if (await _webController.canGoBack()) {
+                  await _webController.goBack();
+                } else {
+                  log('Нет записи в истории');
+                }
+              },
+              icon: const Icon(Icons.arrow_back_ios),
+            ),
+            IconButton(
+              onPressed: () async {
+                if (await _webController.canGoForward()) {
+                  await _webController.goForward();
+                } else {
+                  log('Нет записи в истории');
+                }
+              },
+              icon: const Icon(Icons.arrow_forward_ios),
+            ),
+            IconButton(
+              onPressed: () => _webController.reload(),
+              icon: const Icon(Icons.replay),
+            ),
+            PopupMenuButton<MenuOptions>(
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                  value: MenuOptions.clearCache,
+                  child: Text('Удалить кеш'),
+                ),
+                const PopupMenuItem(
+                  value: MenuOptions.clearCookies,
+                  child: Text('Удалить Cookies'),
+                ),
+              ],
+              onSelected: (value) {
+                switch (value) {
+                  case MenuOptions.clearCache:
+                    _onClearCache(_webController, context);
+                    break;
+                  case MenuOptions.clearCookies:
+                    _onClearCookies(context);
+                    break;
+                }
+              },
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            LinearProgressIndicator(
+              value: progress,
+              color: Colors.red,
+              backgroundColor: Colors.black,
+            ),
+            Expanded(
+              child: WebViewWidget(
+                controller: _webController,
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.next_plan, size: 32),
+          onPressed: () =>
+              _webController.loadRequest(Uri.parse('https://www.youtube.com')),
+        ),
       ),
     );
+  }
+
+  Future<void> _onClearCookies(BuildContext context) async {
+    final bool hasCookies = await WebViewCookieManager().clearCookies();
+    String message = 'Cookies удалены';
+    if (!hasCookies) message = 'Cookies уже были очщены';
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  Future<void> _onClearCache(
+      WebViewController controller, BuildContext context) async {
+    await controller.clearCache();
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text('Кеш очищен')));
   }
 }
