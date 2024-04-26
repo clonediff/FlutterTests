a6bce9a95ac8221362c330c9fd5d1cb7c6b42268
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-26 12:55:16 +0300

    [add] Unique key Example for StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,8 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
 
     tiles = [
-      StatefulColorWidget(text: '1'),
-      StatefulColorWidget(text: '2'),
+      StatefulColorWidget(key: UniqueKey(), text: '1'),
+      StatefulColorWidget(key: UniqueKey(), text: '2'),
     ];
   }
 
@@ -141,10 +141,10 @@ Row Element                                                 Row Element
  ├-- ColorElement 1 (Stateless Element)                      ├-- ColorElement 1 (Stateless Element)
  └-- ColorElement 2 (Stateless Element)                      └-- ColorElement 2 (Stateless Element)
 
-Т.е. Widget'ы переставляются, а Element'ы остаются на месте и переиспользуются, т.к. типы виджетов не поменялось, но привязываются к другим Widget'ам с новыми цветами
+Т.е. Widget'ы переставляются, а Element'ы остаются на месте и переиспользуются, т.к. типы виджетов не поменялись, а ключи не заданы,
+но привязываются к другим Widget'ам с новыми цветами
 
- */
-
+*/
 
 /*
 
@@ -158,7 +158,32 @@ Row Element                                               Row Element
  ├-- ColorElement 1 (Stateful Element) -> State 1          ├-- ColorElement 1 (Stateful Element) -> State 1
  └-- ColorElement 2 (Stateful Element) -> State 2          └-- ColorElement 2 (Stateful Element) -> State 2
 
-Т.е. Widget'ы переставляются, а Element'ы остаются на месте и переиспользуются, т.к. типы виджетов не поменялось, но привязываются к другим Widget'ам
-State'ы привязываются к Element'ам, поэтому то, что в Widget свапаются (text), а то, что в State'ах нет (myColor)
+Т.е. Widget'ы переставляются, а Element'ы остаются на месте и переиспользуются, т.к. типы виджетов не поменялось, а ключи не заданы,
+но привязываются к другим Widget'ам State'ы привязываются к Element'ам, поэтому то, что в Widget свапаются (text), а то, что в State'ах нет (myColor)
+
+*/
+
+/*
+
+Widget Tree:                                              Widget Tree:
+Row Widget                                                Row Widget
+ ├-- ColorWidget 1 (Stateful Widget)                       ├-- ColorWidget 2 (Stateful Widget)
+ |      └-- Widget Key 1                                   |      └-- Widget Key 2
+ └-- ColorWidget 2 (Stateful Widget)                       └-- ColorWidget 1 (Stateful Widget)
+        └-- Widget Key 2                                          └-- Widget Key 1
+                                            --------->
+ElementTree:                                              ElementTree:
+Row Element                                               Row Element
+ ├-- ColorElement 1 (Stateful Element)                     ├-- ColorElement 2 (Stateful Element)
+ |      ├-- State 1                                        |      ├-- State 2
+ |      └-- Element Key 1                                  |      └-- Element Key 2
+ └-- ColorElement 2 (Stateful Element)                     └-- ColorElement 1 (Stateful Element)
+        ├-- State 2                                               ├-- State 1
+        └-- Element Key 2                                         └-- Element Key 1
+
+1) Свапаются Widget'ы в Widget Tree
+2) Начинается проверка по Element Tree:
+    2.1) Проверяется RuntimeType старого виджета, и текущего виджета на текущем уровне. old: ColorWidget  == new: ColorWidget
+    2.2) Проверяется Element Key и Widget key на одном уровне. old: WidgetKey2 != new: ElementKey1   =>   Вызывается свап Element'ов
 
  */
